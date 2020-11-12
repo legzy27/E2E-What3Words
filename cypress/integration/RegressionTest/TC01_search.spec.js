38d8dd9833ad0a0b3f73addab64a4af77eadbefe
Legzy27 <niyo4real2002@yahoo.com>
2020-11-12 08:56:18 +0000

    created two test cases for search functionality

--- cypress/integration/RegressionTest/TC01_search.spec.js
+++ cypress/integration/RegressionTest/TC01_search.spec.js
@@ -1,1 +1,18 @@
-/// <ref
+describe('Search', function(){
+    it('Location Search', function(){
+        cy.visit('https://what3words.com/daring.lion.race')
+        cy.get('[data-testid=CookieNotice-Close]').click()
+        cy.get('#search-popover').type('Bromley{enter}').type('{downarrow}').click();
+    })
+})
+
+
+
+describe('Search Address', function(){
+    it('Location Search With Door Number ', function(){
+        cy.visit('https://what3words.com/daring.lion.race')
+        cy.get('[data-testid=CookieNotice-Close]').click()
+        cy.get('#search-popover').type('78 Marner Point 1 Jefferson Plaza {enter}').type('{downarrow}').click();
+    })
+})
+
